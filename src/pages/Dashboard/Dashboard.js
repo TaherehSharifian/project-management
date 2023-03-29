98a4a74cd60482733ef7941f7cbca24b7c592b60
Tahereh Sharifian <tahereh.sharifian97@gmail.com>
2023-03-29 15:02:49 +0430

    dashboard + project list

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,16 @@
-import './Dashboard.css'
+import "./Dashboard.css";
 
+import { useCollections } from "../../hooks/useCollections";
+import ProjectList from "../../components/ProjectList/ProjectList";
 
 export default function Dashboard() {
+  const { documents, error } = useCollections("projects");
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2 className="page-title">Project List</h2>
+      {error && <div className="error">{error.message}</div>}
+      {documents && <ProjectList projects={documents} />}
     </div>
-  )
+  );
 }
